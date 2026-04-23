4be7f471bb63f53e1ffe6d3fd54eb7480ccde689
Timothé Duquesne <timothe.duquesne@gmail.com>
2026-04-23 21:36:08 +0200

    update: improved data prep, and first ML result

--- backend/utils/ml_utils.py
+++ backend/utils/ml_utils.py
@@ -1,10 +1,32 @@
 import spacy
+import pandas as pd
 
-nlp = spacy.load("fr_core_news_sm")
+nlp = spacy.load("en_core_web_sm")
 
-def clean_text(text):
-    doc = nlp(text.lower())
 
-    tokens = [token.lemma_ for token in doc if not token.is_stop and not token.is_punct and not token.is_space]
+def clean_text(texts):
+    processed_texts = []
 
-    return " ".join(tokens)
+    for doc in nlp.pipe(
+        texts, disable=["parser", "ner"], batch_size=1000, n_process=-1
+    ):
+        tokens = [
+            token.lemma_.lower()
+            for token in doc
+            if not token.is_stop and not token.is_punct and not token.is_space
+        ]
+        processed_texts.append(" ".join(tokens))
+
+    return processed_texts
+
+
+def combine_text_columns(df: pd.DataFrame) -> pd.Series:
+    return (
+        df["Consumer Claim"].fillna("").astype(str)
+        + " "
+        + df["Company"].fillna("").astype(str)
+        + " "
+        + df["State"].fillna("").astype(str)
+        + " "
+        + df["ZIP code"].fillna("").astype(str)
+    )
